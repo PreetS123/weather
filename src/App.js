c2a9b31b2717862b2626cdf447ed4cb3f62b9b40
PreetS123 <preeti_fw15_365@masai.school>
2022-08-06 21:34:02 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import UilReact from '@iconscout/react-unicons/icons/uil-react';
+
 import './App.css';
+import { TopButtons } from './Components/TopButtons';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
+       <TopButtons/>
     </div>
   );
 }
---
+++ src/Components/TopButtons.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const TopButtons = () => {
+
+    const cities=[
+        {
+            id:1,
+            title:'london'
+        },
+        {
+            id:2,
+            title:'london'
+        },
+        {
+            id:3,
+            title:'london'
+        },
+        {
+            id:4,
+            title:'london'
+        },
+        {
+            id:5,
+            title:'london'
+        }
+    ]
+  return (
+    <div>TopButtons</div>
+  )
+}
